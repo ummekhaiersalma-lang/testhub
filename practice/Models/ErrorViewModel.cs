60fadc9bf6b6f7cb1e37f6f9f026be4c7bdbc1a2
md Abdullah <damparasalma@gmail.com>
2025-12-29 12:08:10 +0600

    mv project created

---
+++ practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
